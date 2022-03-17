26f45aca05d9798cd1dc6c40950b6621253add65
Alexey Chudin <kazqvaizer@gmail.com>
2022-03-17 12:02:31 +1000

    The date in response

--- src/actions/where.py
+++ src/actions/where.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from models import CashRemain
 
 from .base import Action
@@ -5,13 +6,17 @@ from .base import Action
 
 class WhereAction(Action):
     def do(self):
-        remains = CashRemain.select().order_by(CashRemain.amount.desc())
+        remains = list(CashRemain.select().order_by(CashRemain.amount.desc()))
 
         if len(remains) == 0:
             self.reply("Валюты нигде нет. Добро пожаловать в Союз.")
             return
 
-        self.reply("Вот, все что есть:\n\n")
+        availability_date = remains[0].created
+        availability_date += timedelta(hours=10)  # Vladivostok time
+        availability_date = availability_date.strftime("%Y-%m-%d %H:%M:%S")
+
+        self.reply(f"Вот, все что есть на {availability_date}\n\n")
         self.reply(
             "\n".join(
                 [
